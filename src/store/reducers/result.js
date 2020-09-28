492f2d9e2209f06c6f98a4e22cab15d47324ca6c
Antony Chiramel <antonychiramel07@gmail.com>
2020-09-28 20:44:29 +0530

    Redux Basics Complete

---
+++ src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../actions";
+
+const initialState = {
+  results: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat(action.result),
+      };
+    case actionTypes.DELETE_RESULT:
+      const id = action.resultElId;
+      const newResults = state.results.filter((result, index) => {
+        return index !== id;
+      });
+      return {
+        ...state,
+        results: newResults,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
